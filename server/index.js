3b1d514af2c20250940fce0f0975ba4ffcbd3fda
DANISH <124383478+Mddanish4338@users.noreply.github.com>
2025-06-06 16:26:23 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,91 +1,3 @@
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const cors = require("cors");
-// const bcrypt = require("bcrypt");
-// const jwt = require("jsonwebtoken");
-// const cookieParser = require("cookie-parser");
-// const UserModel = require("./models/User");
-
-// const app = express();
-// app.use(express.json());
-// app.use(
-//   cors({
-//     origin: ["http://localhost:3000"],
-//     methods: ["GET", "POST"],
-//     credentials: true,
-//   })
-// );
-// app.use(cookieParser());
-
-// mongoose
-//   .connect(
-//     "mongodb+srv://MDDanish4338:RyKSvpudfrPm9HzN@authentication.nniuham.mongodb.net/register"
-//   )
-//   .then(() => console.log("DB connected"))
-//   .catch((err) => console.log(err));
-
-// const varifyUser = (req, res, next) => {
-//   const token = req.cookies.token;
-//   if (!token) {
-//     return res.json("Token is missing");
-//   } else {
-//     jwt.verify(token, "jwt-secret-key", (err, decoded) => {
-//       if (err) {
-//         return res.json("Error with token");
-//       } else {
-//         if (decoded.role === "admin") {
-//           next();
-//         } else {
-//           return res.json("not admin");
-//         }
-//       }
-//     });
-//   }
-// };
-
-// app.get("/dashboard", varifyUser, (req, res) => {
-//   res.json("Success");
-// });
-
-// app.post("/register", (req, res) => {
-//   const { name, email, password } = req.body;
-//   bcrypt
-//     .hash(password, 10)
-//     .then((hash) => {
-//       UserModel.create({ name, email, password: hash })
-//         .then((user) => res.json("Success"))
-//         .catch((err) => res.json(err));
-//     })
-//     .catch((err) => res.json(err));
-// });
-
-// app.post("/login", (req, res) => {
-//   const { email, password } = req.body;
-//   UserModel.findOne({ email: email }).then((user) => {
-//     if (user) {
-//       bcrypt.compare(password, user.password, (err, response) => {
-//         if (response) {
-//           const token = jwt.sign(
-//             { email: user.email, role: user.role },
-//             "jwt-secret-key",
-//             { expiresIn: "1d" }
-//           );
-//           res.cookie("token", token);
-//           return res.json({ Status: "Success", role: user.role });
-//         } else {
-//           return res.json("The password is incorrect");
-//         }
-//       });
-//     } else {
-//       return res.json("No record existed");
-//     }
-//   });
-// });
-
-// app.listen(3001, () => {
-//   console.log("server is Running");
-// });
-
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -100,7 +12,7 @@ app.use(cors({ origin: "http://localhost:3000", methods: ["GET", "POST"], creden
 app.use(cookieParser());
 
 mongoose
-  .connect("mongodb+srv://MDDanish4338:RyKSvpudfrPm9HzN@authentication.nniuham.mongodb.net/register", { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect("", { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("DB connected"))
   .catch((err) => console.log("DB Connection Error:", err));
 
